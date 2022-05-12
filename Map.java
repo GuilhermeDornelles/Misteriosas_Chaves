76c9cfdaa8f5c0fbc1aabbec8472fcb510aa1afd
Guilherme Matheus Dornelles <81441745+GuilhermeDornelles@users.noreply.github.com>
2022-05-11 23:00:25 -0300

    Iniciando lógica de mapa

--- Map.java
+++ Map.java
@@ -1,11 +1,33 @@
+import java.util.ArrayList;
+
+import javafx.geometry.Pos;
+
 //The class Map works like a Graph but with some changes
 public class Map {
+    ArrayList<Position> players = new ArrayList<>();
 
     public Map() {
 
     }
 
-    public int walk() {
+    public int walk(Position player) {
+
+        player.markPosition();
+        ArrayList<Position> list = new ArrayList<Position>();
+        list.add(player)
+        while(!list.isEmpty()){
+            //v=primeiro(L)
+            visita v
+            for(Position neighbor: )
+        }
+
         return -1;
+    }
+
+    public String results(){
+        //Sum to the String each result of each player
+        for(Position player : players){
+            walk(map, Players)
+        }
     }
 }
