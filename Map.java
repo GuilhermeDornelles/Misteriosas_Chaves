9efb6331a7b92ba42eb2ffd4009e0bd4ecabb553
Guilherme Matheus Dornelles <81441745+GuilhermeDornelles@users.noreply.github.com>
2022-05-19 02:51:36 -0300

    Finalizando método Map( )

--- Map.java
+++ Map.java
@@ -3,143 +3,200 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-
-import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
-
-import javafx.geometry.Pos;
+import java.util.Comparator;
 
 //The class Map works like a Graph but with some changes
 public class Map {
-    ArrayList<Position> players = new ArrayList<>();
-    /*
-     * public Map(File file) {
-     * ArrayList<String> lines = new ArrayList<>();
-     * try {
-     * FileReader arq = new FileReader(file);
-     * BufferedReader lerArq = new BufferedReader(arq);
-     * String line = "hi";
-     * while (line != null) {
-     * line = lerArq.readLine();
-     * if (line != null) {
-     * lines.add(line);
-     * }
-     * }
-     * arq.close();
-     * } catch (IOException e) {
-     * System.out.printf("Error while reading the file: %s.\n", e.getMessage());
-     * }
-     * 
-     * //Transform the lines in many nodes and connections
-     * for(int lNum=0; lNum<lines.size(); lNum++){
-     * String l = lines.get(lNum);
-     * for(int i=0; i<l.length(); i++){
-     * //The line is the first one
-     * if(lines.get(0) == l){
-     * Character position = l.charAt(i);
-     * if(position != '#'){
-     * switch(position){
-     * case '.' : Position node = new Position(type, label)
-     * break;
-     * }
-     * 
-     * }
-     * }
-     * //The line is the last one
-     * else if(lines.get(lines.size()-1) == l){
-     * 
-     * }
-     * //The line is on the middle
-     * else {
-     * 
-     * }
-     * }
-     * }
-     * }
-     */
-    /*
-     * private ArrayList<Integer> getNeighbors(ArrayList<String> lines, int
-     * indexLine, int indexChar, Position position) {
-     * if (indexLine < lines.size()) {
-     * 
-     * }
-     * 
-     * ArrayList<Position> listOfNeighbors = new ArrayList<>();
-     * 
-     * // Can't add above because has been read already
-     * /*
-     * if (indexLine > 0) {
-     * // Add neighbor in line above
-     * Character above = lines.get(indexLine - 1).charAt(indexChar);
-     * if (above != '#') {
-     * Position neighbor = new Position(above);
-     * neighbor.addNeighbor(position.getId());
-     * position.addNeighbor(neighbor.getId());
-     * }
-     * }
-     */
-    /*
-     * if (indexChar > 0) {
-     * // Add previous
-     * Character previous = lines.get(indexLine).charAt(indexChar - 1);
-     * if (previous != '#') {
-     * Position neighbor = new Position(previous);
-     * neighbor.addNeighbor(position.getId());
-     * position.addNeighbor(neighbor.getId());
-     * }
-     * }
-     * if (indexChar < lines.get(indexLine).length() - 1) {
-     * // Add next
-     * Character next = lines.get(indexLine).charAt(indexChar + 1);
-     * if (next != '#') {
-     * Position neighbor = new Position(next);
-     * neighbor.addNeighbor(position.getId());
-     * position.addNeighbor(neighbor.getId());
-     * }
-     * }
-     * if (indexLine < lines.size() - 1) {
-     * // Add below
-     * Character below = lines.get(indexLine + 1).charAt(indexChar);
-     * if (below != '#') {
-     * Position neighbor = new Position(below);
-     * neighbor.addNeighbor(position.getId());
-     * position.addNeighbor(neighbor.getId());
-     * }
-     * }
-     * 
-     * }
-     */
+    private ArrayList<Position> players;
+    private ArrayList<Position> markedPositions;
+    // After the running, pass through this list to see if exists doors to unlock
+    private ArrayList<Position> listOfDoors;
+    private ArrayList<Position> listOfKeys;
 
-    /*
-     * public int walk(Position player) {
-     * // MarkOff all the nodes
-     * // TODO
-     * // Start with 1 because player position counts
-     * int count = 1;
-     * player.markPosition();
-     * ArrayList<Position> list = new ArrayList<Position>();
-     * list.add(player);
-     * while (!list.isEmpty()) {
-     * Position v = list.get(0);
-     * // TODO
-     * // visita v;
-     * for (Position position : v.neighbors) {
-     * position.markPosition();
-     * list.add(position);
-     * }
-     * list.remove(0);
-     * }
-     * 
-     * return -1;
-     * 
-     * }
-     */
+    public Map(File file) {
+        this.players = new ArrayList<>();
+        this.markedPositions = new ArrayList<>();
+        this.listOfDoors = new ArrayList<>();
+        this.listOfKeys = new ArrayList<>();
+        ArrayList<String> lines = new ArrayList<>();
+        try {
+            FileReader arq = new FileReader(file);
+            BufferedReader lerArq = new BufferedReader(arq);
+            String line = "hi";
+            while (line != null) {
+                line = lerArq.readLine();
+                if (line != null && line.length() == 0) {
+                    break;
+                }
+                if (line != null) {
+                    lines.add(line);
+                }
+            }
+            arq.close();
+        } catch (IOException e) {
+            System.out.printf("Error while reading the file: %s.\n", e.getMessage());
+        }
+
+        int width = lines.get(0).length();
+        int height = lines.size();
+
+        Position[][] matrix = new Position[height][width];
+
+        for (int h = 0; h < height; h++) {
+            String line = lines.get(h);
+            for (int w = 0; w < width; w++) {
+                matrix[h][w] = new Position(line.charAt(w));
+                if (Character.isDigit(matrix[h][w].getLabel())) {
+                    players.add(matrix[h][w]);
+                }
+            }
+        }
+        ArrayList<Position> list = transformToGraph(matrix);
+    }
+
+    // Transform matrix to graph
+    private ArrayList<Position> transformToGraph(Position[][] matrix) {
+        ArrayList<Position> positions = new ArrayList<>();
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        // Connects the first line
+        // Pass from the second column beacuse the first one is always wall
+        // Until the last but one, because the last is always wall
+        for (int i = 1; i < columns - 1; i++) { // For each column
+            // Always connecting with the next Position
+            Position current = matrix[1][i];
+            if (!(current.getType() == Type.WALL)) {
+                positions.add(current);
+                Position next = matrix[1][i + 1];
+                if (!(i == columns - 2) && !(next.getType() == Type.WALL)) {
+                    // Connect the nodes
+                    current.addNeighbor(next);
+                    next.addNeighbor(current);
+                }
+            }
+        }
+
+        // Connect the others lines with the next node and the above
+        // Pass from the second line until the last but one because the last has only
+        // walls
+        for (int h = 2; h < rows - 1; h++) { // For each row
+            // Pass from the second column beacuse the first one is always wall
+            for (int w = 1; w < columns - 1; w++) {// For each column
+                Position current = matrix[h][w];
+                if (!(current.getType() == Type.WALL)) {
+                    positions.add(current);
+                    Position next = matrix[h][w + 1];
+                    if (!(w == columns - 2) && !(next.getType() == Type.WALL)) {
+                        // Connect the nodes
+                        current.addNeighbor(next);
+                        next.addNeighbor(current);
+                    }
+                    Position above = matrix[h - 1][w];
+                    if (above.getType() != Type.WALL) {
+                        // Connect the nodes
+                        current.addNeighbor(above);
+                        above.addNeighbor(current);
+                    }
+                }
+            }
+        }
+        return positions;
+    }
+
+    private int walk(Position position) {
+        // It means that the method is called by a player and it's needed to markOff all
+        // the nodes
+        if (position.getType() == Type.PLAYER) {
+            beginWalk();
+            System.out.println("Jogando com o player: " + position.getLabel());
+        }
+
+        ArrayList<Position> list = new ArrayList<Position>();
+
+        // This number will be returned meaning the number of positions
+        // that the node can "walk"
+        int count = 0;
+        position.markPosition(true);
+        markedPositions.add(position);
+        list.add(position);
+        while (!list.isEmpty()) {
+            Position v = list.get(0);
+            // Counts the position
+            count++;
+            for (Position neighbor : v.getNeighbors()) {
+                if (!neighbor.IsMarked()) {
+                    if (neighbor.getType() == Type.KEY) {
+                        System.out.println("Chave encontrada: " + neighbor.getLabel());
+                        listOfKeys.add(neighbor);
+                        neighbor.markPosition(true);
+                        markedPositions.add(neighbor);
+                        list.add(neighbor);
+                    } else if (neighbor.getType() == Type.DOOR) {
+                        System.out.println("Porta encontrada: " + neighbor.getLabel());
+                        listOfDoors.add(neighbor);
+                    } else {
+                        neighbor.markPosition(true);
+                        markedPositions.add(neighbor);
+                        list.add(neighbor);
+                    }
+                }
+
+            }
+            list.remove(0);
+        }
+
+        if (!listOfDoors.isEmpty()) {
+            for (Position door : listOfDoors) {
+                // Verify if there is a key for each door
+                if (!door.isAccessible()) {
+                    for (Position key : listOfKeys) {
+                        // The actual key is for the actual door
+                        if (Character.toLowerCase(door.getLabel()) == key.getLabel()) {
+                            door.markPosition(true);
+                            markedPositions.add(door);
+                            door.setAcessible(true);
+                            listOfDoors.remove(door);
+                            count += walk(door);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    // beginWalk will prepare the lists to start a new walk in the map
+    private void beginWalk() {
+        clearMarkedPositions();
+        clearListOfDoors();
+        clearListOfKeys();
+    }
+
+    private void clearMarkedPositions() {
+        for (Position p : markedPositions) {
+            p.markPosition(false);
+        }
+        markedPositions.clear();
+    }
+
+    private void clearListOfKeys() {
+        listOfKeys.clear();
+    }
+
+    private void clearListOfDoors() {
+        listOfDoors.clear();
+    }
+
+    private void sortPLayers() {
+        players.sort((x, y) -> x.getLabel());
+    }
+
     public String results() {
-        // Sum to the String each result of each player
-        /*
-         * for(Position player : players){
-         * walk(map, Players)
-         * }
-         */
-        return "";
+        String results = "Results\n";
+        for (Position player : players) {
+            results += "Players " + player.getLabel() + " pode percorrer por " + walk(player) + " posições!\n";
+        }
+        return results;
     }
 }
