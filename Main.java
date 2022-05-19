5f183468617f373546add19d8b459deb71fecfe8
Guilherme Matheus Dornelles <81441745+GuilhermeDornelles@users.noreply.github.com>
2022-05-19 02:51:05 -0300

    Jeito primitivo para ler todos os casos de testes

--- Main.java
+++ Main.java
@@ -1,41 +1,41 @@
+import java.io.File;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Position> list = addPositions();
-        for (Position p : list) {
-            System.out.println(p.toString());
-        }
-    }
 
-    public static ArrayList<Position> addPositions() {
-        ArrayList<Character> listDoors = new ArrayList<Character>();
-        listDoors.add('A');
-        listDoors.add('B');
-        listDoors.add('C');
+        String dir0 = "casoFDS.txt";
+        File file0 = new File(dir0);
+        Map mapa0 = new Map(file0);
 
-        ArrayList<Character> listKeys = new ArrayList<Character>();
-        listKeys.add('a');
-        listKeys.add('b');
+        // String dir1 = "casoTeste.txt";
+        // File file1 = new File(dir1);
+        // Map mapa1 = new Map(file1);
 
-        ArrayList<Position> list = new ArrayList<Position>();
-        for (Character c : listDoors) {
-            list.add(new Position(c));
-        }
-        for (Character c : listKeys) {
-            list.add(new Position(c));
-        }
-        for (int i = 0; i < 6; i++) {
-            list.add(new Position('.'));
-        }
-        return list;
-    }
+        // String dir2 = "caso05.txt";
+        // File file2 = new File(dir2);
+        // Map mapa2 = new Map(file2);
 
-    public static boolean isAcessible(char c, ArrayList<Character> list) {
-        boolean accesible = false;
-        if (list.contains(Character.toLowerCase(c))) {
-            accesible = true;
-        }
-        return accesible;
+        // String dir3 = "caso06.txt";
+        // File file3 = new File(dir3);
+        // Map mapa3 = new Map(file3);
+
+        // String dir4 = "caso07.txt";
+        // File file4 = new File(dir4);
+        // Map mapa4 = new Map(file4);
+
+        // String dir5 = "caso08.txt";
+        // File file5 = new File(dir5);
+        // Map mapa5 = new Map(file5);
+
+        // String dir6 = "caso09.txt";
+        // File file6 = new File(dir6);
+        // Map mapa6 = new Map(file6);
+
+        // String dir7 = "caso10.txt";
+        // File file7 = new File(dir7);
+        // Map mapa7 = new Map(file7);
+
+        System.out.println(mapa0.results());
     }
 }
